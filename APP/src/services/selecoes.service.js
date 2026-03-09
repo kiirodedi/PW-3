07345bd62dba9d998d45634b6f679ae3c6490cb4
kiirodedi <moraismoralesdeborafernanda3@gmail.com>
2026-03-09 20:03:26 -0300

    criação dos arquivos

--- APP/src/services/selecoes.service.js
+++ APP/src/services/selecoes.service.js
@@ -1,3 +1,24 @@
-const selecoesListar = ()=>{
-    
-}
+import { routes } from "../config/routes.js";
+import { GET, POST, DELETE, PUT, PATCH } from "./http.js";
+
+const getSelecoes = async () => {
+    return await GET(routes.selecoes);
+};
+
+const postSelecao = async (data) => {
+    return await POST(routes.selecoes, data);
+};
+
+const putSelecao = async (id, data) => {
+    return await PUT(`${routes.selecoes}/${id}`, data);
+};
+
+const patchSelecao = async (id, data) => {
+    return await PATCH(`${routes.selecoes}/${id}`, data);
+};
+
+const deleteSelecao = async (id) => {
+    return await DELETE(`${routes.selecoes}/${id}`);
+};
+
+export { getSelecoes, postSelecao, putSelecao, patchSelecao, deleteSelecao };
