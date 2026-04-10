318333976c32967e5a2c74f33d44d0ca19617930
DÉBORA FERNANDA MORAIS MORALES <debora.morales01@etec.local>
2026-04-10 08:39:10 -0300

    slasla

--- suhde/App.tsx
+++ suhde/App.tsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
 import { VeiculosList } from "./src/pages/VeiculosList";
+import { ListaVazia } from "./src/pages/ListaVazia";
 
 export default function App() {
-  return <VeiculosList />;
+  // 1. Aqui você teria o seu estado com os carros
+  // Se for [], ele renderiza a ListaVazia. Se tiver itens, renderiza a VeiculosList.
+  const [veiculos, setVeiculos] = useState([]); 
+
+  // 2. A estrutura do IF organizada:
+  if (veiculos.length === 0) {
+    return <ListaVazia />;
+  }
+
+  return <VeiculosList dados={veiculos} />;
 }
